9b0a86a518a914ff3d612061611f5cde31210b15
bigbass1997 <bigbass1997.website@gmail.com>
2022-08-26 23:44:00 -0500

    Implemented structured replay device communication protocol.

--- software/src/replay.rs
+++ software/src/replay.rs
@@ -1,11 +1,12 @@
-use std::io::{Read, Write};
+use std::path::PathBuf;
 use std::time::Duration;
-use bytes::{Buf, Bytes};
 use clap::ArgMatches;
-use log::{error, info};
+use log::{error, info, warn};
 use serialport::ClearBuffer;
+use tasd::spec::{ConsoleType, InputChunk, KEY_CONSOLE_TYPE, KEY_INPUT_CHUNK, TasdMovie};
+use crate::replay::comms::{Device, Response, System, VeritasMode};
 
-mod device;
+mod comms;
 
 pub fn handle(matches: &ArgMatches) {
     if matches.is_present("list-devices") {
@@ -16,20 +17,77 @@ pub fn handle(matches: &ArgMatches) {
         return;
     }
     
-    let movie = std::fs::read(matches.value_of("movie").unwrap()).unwrap();
-    let mut device = {
-        let port_name = matches.value_of("device").unwrap();
-        
-        let mut device = None;
-        for port in serialport::available_ports().unwrap_or_default() {
-            if port.port_name == port_name {
-                device = Some(serialport::new(port.port_name, 500000).timeout(Duration::from_secs(10)).open().unwrap());
-                break;
-            }
+    let mut dev = Device::new(matches.value_of("device").unwrap(), 500000, Duration::from_secs(10)).unwrap();
+    dev.clear(ClearBuffer::All);
+    
+    let tasd = TasdMovie::new(&PathBuf::from(matches.value_of("movie").unwrap())).expect("Failed to parse movie.");
+    let console = tasd.search_by_key(vec![KEY_CONSOLE_TYPE]).first().expect("No console type provided in TASD. Cannot continue.").as_any().downcast_ref::<ConsoleType>().unwrap();
+    let inputs: Vec<u8> = {
+        let chunks: Vec<&[u8]> = tasd.search_by_key(vec![KEY_INPUT_CHUNK]).iter().map(|packet| packet.as_any().downcast_ref::<InputChunk>().unwrap().inputs.as_slice()).collect();
+        let mut inputs = vec![];
+        for chunk in chunks {
+            inputs.extend_from_slice(chunk);
         }
         
-        device
-    }.unwrap();
+        inputs
+    };
+
+    {
+        let pong = dev.ping();
+        if pong != Response::Pong {
+            error!("Failed to ping device. {:?}", pong);
+            return;
+        }
+    }
+
+    info!("{}", dev.get_status());
+    
+    match console.kind.into() {
+        System::Nes => {
+            let mut ptr = 0usize;
+            let mut has_started = false;
+            
+            println!("Prefilling buffer...");
+            while ptr < inputs.len() {
+                let input = &inputs[ptr..(ptr + 2)];
+                let (res, system, data) = dev.provide_input(System::Nes, input);
+                
+                if system != System::Nes {
+                    warn!("Possible communication error! System doesn't match: (sent) {:?} vs (recv) {:?}", System::Nes, system);
+                }
+                if data != input {
+                    warn!("Possible communication error! Inputs don't match: (sent) {} vs (recv) {}", hex::encode_upper(input), hex::encode_upper(data));
+                }
+                
+                match res {
+                    Response::Ok => ptr += 2,
+                    Response::BufferFull if !has_started => {
+                        has_started = true;
+                        
+                        let res = dev.set_replay_mode(VeritasMode::ReplayNes);
+                        if res != Response::Ok {
+                            error!("Failed to set replay mode! {:?}", res);
+                            return;
+                        }
+                        info!("Starting replay.")
+                    },
+                    Response::BufferFull => (),
+                    
+                    _ => warn!("Error occurred while sending inputs: {:?}", res)
+                }
+            }
+        },
+        System::N64 => {
+            todo!()
+        },
+        System::Genesis => {
+            todo!()
+        },
+        System::A2600 => {
+            todo!()
+        },
+        _ => unimplemented!()
+    }
     
     /*let version = u32::from_be_bytes((&movie[4..8]).try_into().unwrap());
     let start = if version == 1 || version == 2 { 0x200 } else { 0x400 };
@@ -78,7 +136,7 @@ pub fn handle(matches: &ArgMatches) {
         }
     }*/
     
-    device.clear(ClearBuffer::All).unwrap_or_default();
+    /*device.clear(ClearBuffer::All).unwrap_or_default();
     device.write_all(&[0x04]).unwrap();
     let mut buf = [0u8];
     device.read_exact(&mut buf).unwrap();
@@ -104,5 +162,5 @@ pub fn handle(matches: &ArgMatches) {
         }
     }
     
-    info!("Buffer filling complete!");
+    info!("Buffer filling complete!");*/
 }
---
+++ software/src/replay/comms.rs
@@ -0,0 +1,140 @@
+use std::io::{Read, Write};
+use std::time::Duration;
+use log::error;
+use num_enum::{IntoPrimitive, FromPrimitive};
+use serialport::{ClearBuffer, SerialPort};
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, IntoPrimitive, FromPrimitive)]
+#[repr(u8)]
+pub enum Command {
+    SetReplayMode = 0x01,
+    ProvideInput = 0x02,
+    GetStatus = 0x03,
+    
+    Ping = 0xAA,
+    
+    #[num_enum(default)]
+    Invalid = 0x00,
+}
+
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, IntoPrimitive, FromPrimitive)]
+#[repr(u8)]
+pub enum Response {
+    Ok = 0x01,
+    Text = 0x02,
+    BufferFull = 0xF0,
+    
+    Pong = 0x55,
+    
+    #[num_enum(default)]
+    Err = 0x00,
+}
+
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, IntoPrimitive, FromPrimitive)]
+#[repr(u8)]
+pub enum System {
+    Nes = 0x01,
+    //Snes = 0x02,
+    N64 = 0x03,
+    Genesis = 0x08,
+    A2600 = 0x09,
+    #[num_enum(default)]
+    Unknown = 0xFF,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone, IntoPrimitive, FromPrimitive)]
+#[repr(u8)]
+pub enum VeritasMode {
+    Initial = 0x00,
+    #[num_enum(default)]
+    Idle = 0x01,
+    ReplayN64 = 0x02,
+    ReplayNes = 0x03,
+    ReplayA2600 = 0x04,
+    ReplayGenesis = 0x05,
+}
+
+pub struct Device {
+    inner: Box<dyn SerialPort>,
+}
+impl Device {
+    pub fn new<S: AsRef<str>>(port_name: S, baud: u32, timeout: Duration) -> Option<Self> {
+        let mut inner = None;
+        for port in serialport::available_ports().unwrap_or_default() {
+            if port.port_name == port_name.as_ref() {
+                inner = Some(serialport::new(port.port_name, baud).timeout(timeout).open().unwrap());
+                break;
+            }
+        }
+        if inner.is_none() {
+            return None;
+        }
+        
+        Some(Self {
+            inner: inner.unwrap()
+        })
+    }
+    
+    pub fn set_replay_mode(&mut self, mode: VeritasMode) -> Response {
+        self.write(&[Command::SetReplayMode.into(), mode.into()]);
+        
+        self.read_u8().into()
+    }
+    
+    pub fn provide_input(&mut self, system: System, data: &[u8]) -> (Response, System, Vec<u8>) {
+        self.write(&[&[Command::ProvideInput.into(), system.into()], data].concat());
+        let res = self.read(data.len() + 2);
+        
+        (res[0].into(), res[1].into(), res[2..].to_vec())
+    }
+    
+    pub fn get_status(&mut self) -> String {
+        self.write_u8(Command::GetStatus.into());
+        let res = self.read(5);
+        
+        if Response::from(res[0]) != Response::Text {
+            error!("Invalid GetStatus response!");
+            
+            String::new()
+        } else {
+            let res = self.read(u32::from_be_bytes(res[1..].try_into().unwrap()) as usize);
+            
+            String::from_utf8_lossy(&res).to_string()
+        }
+    }
+    
+    pub fn ping(&mut self) -> Response {
+        self.write_u8(Command::Ping.into());
+        
+        self.read_u8().into()
+    }
+    
+    
+    pub fn clear(&self, buffer: ClearBuffer) {
+        self.inner.clear(buffer).unwrap();
+    }
+    
+    pub fn read_u8(&mut self) -> u8 {
+        let mut buf = [0u8];
+        self.inner.read_exact(&mut buf).unwrap();
+        
+        buf[0]
+    }
+    
+    pub fn read(&mut self, len: usize) -> Vec<u8> {
+        let mut buf = vec![0u8; len];
+        self.inner.read_exact(&mut buf).unwrap();
+        
+        buf
+    }
+    
+    pub fn write_u8(&mut self, data: u8) {
+        self.inner.write_all(&[data]).unwrap();
+    }
+    
+    pub fn write(&mut self, data: &[u8]) {
+        self.inner.write_all(data).unwrap();
+    }
+}
